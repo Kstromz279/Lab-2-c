41cce97a53f6d978f9bd54971a31adfaaa8d81dc
Khalil Stroman <7208392@philasd.org>
2020-09-09 23:31:27 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,44 @@
+// Author: Khalil Stroman kzs5955@psu.edu
+// Collaborator:
+// Collaborator:
+// Collaborator:
+// Section: 1
+// Breakout: 12
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  
+ char *grade = readline("Enter your CMPSC 131 grade: \n");
+double g = atof(grade);
+if (g >= 93){
+  printf("Your letter grade for CMPSC 131 is A.\n");
+}
+else if (90 <= g && g < 93){
+  printf("Your letter grade for CMPSC 131 is A-.\n");
+}
+else if (87 <= g && g < 90){
+  printf("Your letter grade for CMPSC 131 is B+.\n");
+}
+else if (83 <= g && g < 87){
+  printf("Your letter grade for CMPSC 131 is B.\n");
+}
+else if (80 <= g && g < 83){
+  printf("Your letter grade for CMPSC 131 is B-.\n");
+}
+else if (77 <= g && g < 80){
+  printf("Your letter grade for CMPSC 131 is C+.\n");
+}
+else if (70 <= g && g < 77){
+  printf("Your letter grade for CMPSC 131 is C.\n");
+}
+else if (60 <= g && g < 70){
+  printf("Your letter grade for CMPSC 131 is D.\n");
+}
+else {
+  printf("Your letter grade for CMPSC 131 is F.\n");
+}
+
+  return 0;
+}
